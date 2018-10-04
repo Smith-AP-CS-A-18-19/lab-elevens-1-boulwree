f605465cb956bbe938141067cd552c3251a4746f
Reece Boulware <boulwree@berkeleyprep.org>
2018-10-04 09:08:59 -0400

    cards

--- Card.java
+++ Card.java
@@ -33,6 +33,10 @@ public class Card {
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		suit = cardSuit;
+		rank = cardRank;
+		pointValue = cardPointValue;
+
 	}
 
 
@@ -42,6 +46,7 @@ public class Card {
 	 */
 	public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return suit;
    }
 
 	/**
@@ -50,6 +55,7 @@ public class Card {
 	 */
 	public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank;
 	}
 
    /**
@@ -58,6 +64,7 @@ public class Card {
 	 */
 	public int pointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -68,6 +75,12 @@ public class Card {
 	 */
 	public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		if (suit == otherCard.suit && pointValue == otherCard.pointValue && rank == otherCard.rank){
+			return true;
+		}else{
+			return false;
+		}
+
 	}
 
 	/**
@@ -83,5 +96,7 @@ public class Card {
 	@Override
 	public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return "The rank is " + rank + " The suit is " + suit + " The pointValue is " + pointValue;
+
 	}
 }
--- CardTester.java
+++ CardTester.java
@@ -9,5 +9,11 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c = new Card("ace" , "club", 1);
+		Card s = new Card ("7", "heart" , 7);
+		Card x = new Card ("7", "heart", 7);
+		System.out.println(x.matches(s));
+		System.out.println(c.matches(x));
+		System.out.println(c.toString());
 	}
 }
